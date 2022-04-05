08aeeba2af28c581c25324808580359ea80076ed
joel <joelkalema63@gmail.com>
2022-04-05 16:05:02 +0200

    Modified the files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import './style.css';
+import { scoreData } from './modules/scoreData';
+
+const listShow = document.querySelector('.scors');
+const display = () => {
+  scoreData.map((list) => {
+    const li = `<div class="score">
+                    <p>${list.name} :</p>
+                    <p class="number">${list.score}</p>
+                  </div>`;
+    listShow.innerHTML += li;
+  })
+}
+
+display();
  
