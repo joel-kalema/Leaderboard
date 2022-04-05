e3e182e545cd8369d50e7a855bdedd3b56a1c905
joel <joelkalema63@gmail.com>
2022-04-05 16:03:33 +0200

    Added folder and scoreData file

---
+++ src/modules/scoreData.js
@@ -0,0 +1,18 @@
+export const scoreData = [
+  {
+    name: 'joel',
+    score: '120'
+  },
+  {
+    name: 'jean-marc',
+    score: '1020'
+  },
+  {
+    name: 'chris',
+    score: '140'
+  },
+  {
+    name: 'patient',
+    score: '400'
+  },
+]
